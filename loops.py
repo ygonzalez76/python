d598b4325c14835316380d14245d84afda941e56
Gonzalez <ygonzalez76@cps.k12.il.us>
2024-05-06 12:13:41 -0500

    first commit

---
+++ loops.py
@@ -0,0 +1,33 @@
+# nums = [1,2,3,4,5]
+
+# for num in nums:
+#     #nestedforloop
+#     for letter in 'abc':
+#         print(num,letter)
+
+# for num in nums:
+#     if num == 3:
+#         print('Found!')
+#         continue
+#     print(num)
+
+# # ranges
+
+# for i in range (1,421):
+#     print(i)
+
+# x = 0
+# while True:
+#     if x == 5:
+#         break
+#     print(x)
+#     x += 1
+
+while True:
+    print("Enter your name:")
+    name = input()
+    if name == 'Yailyn':
+        print("your name is" +name)
+        break
+    else:
+        print("Thank you!")
